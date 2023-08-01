e60768d165364adfb2e618715072d46f8454ae43
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-08-01 00:11:05 -0300

    :recycle: renaming some TUICord methods

--- tuicord/main.py
+++ tuicord/main.py
@@ -12,18 +12,27 @@ load_dotenv()
 
 class TUICord:
     def __init__(self, token: str):
+        """
+        The __init__ function is called when the class is instantiated.
+        It sets up the Discord client and interface, as well as setting a
+        title for the interface.
+
+        :param self: Represent the instance of the class
+        :param token: str: Store the token for the bot
+        :return: A DiscordClient object
+        """
         self.discord_client: DiscordClient = DiscordClient()
         self.interface: Interface = Interface(self.discord_client)
         self.interface.title = 'TUICord'
         self._discord_token: str = token
 
-    def run_bot(self, loop):
+    def run_client(self, loop):
         loop.run_until_complete(
             self.discord_client.run(self._discord_token))
 
-    def start(self):
+    def run_interface(self):
         threading.Thread(
-            target=self.run_bot,
+            target=self.run_client,
             args=[asyncio.new_event_loop()],
             daemon=True
         ).start()
@@ -54,7 +63,7 @@ def run(token: str) -> None:
     """
     click.echo(click.style('Starting...', fg='green'))
     app = TUICord(token)
-    app.start()
+    app.run_interface()
 
 
 if __name__ == '__main__':
