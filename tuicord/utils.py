4f8a36c19b2e74dbf979c770005eb950d4790fd5
Robert Nogueira <robertlucasnogueira@gmail.com>
2023-08-01 00:17:48 -0300

    :memo: update docstrings

--- tuicord/utils.py
+++ tuicord/utils.py
@@ -5,6 +5,13 @@ import discord
 
 
 def format_name(name: str):
+    """
+    The format_name function takes a string as input and returns the same
+    string with all non-alphabetic characters removed.
+
+    :param name: str: Name to be passed into the function
+    :return: A formatted version of the name parameter
+    """
     for char in name:
         if char not in string.ascii_letters + ' ':
             name = name.replace(char, '')
@@ -12,6 +19,16 @@ def format_name(name: str):
 
 
 async def consume_history(channel) -> list[discord.Message]:
+    """
+    The consume_history function takes a channel as an argument and returns a
+    list of messages in that channel.
+    The function uses the async for loop to iterate over the history of
+    the given channel, which is then reversed
+    and returned as a list.
+
+    :param channel: Specify the channel to get the history from
+    :return: A list of messages, in reverse order
+    """
     return list(reversed([message async for message in channel.history()]))
 
 
